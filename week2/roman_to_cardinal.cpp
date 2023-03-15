483f29778e1de78429bc4e7eca85ec98e0090161
jun <wnsgudwnsgud4724@gmail.com>
2023-03-15 20:00:26 +0900

    [BJH] 로마자를 입력받아 정수로 출력하기_실습

--- week2/roman_to_cardinal.cpp
+++ week2/roman_to_cardinal.cpp
@@ -21,7 +21,8 @@ int main() {
     string roman;
     char c;
     
-    cout << "[로마자를 입력하세요]: ";
+    cout << "[로마자를 입력하세요 ";
+    cout << " {I=1},{V=5},{X=10},{L=50},{C=100},{D=500},{M=1000}]:  ";
     while (true) {
         c = _getch();
         if (c == 27) { // ESC키를 누르면 프로그램 종료
@@ -29,7 +30,15 @@ int main() {
         }
         else if (isalpha(c)) { // 알파벳이 입력될 경우
             roman += c;
-            cout << c;
+            if (c >= 'a' && c <= 'z') // 입력된 문자가 영어 소문자인 경우
+            {
+                cout << "오류발생 대문자를 입력하세요: " << endl;
+            }
+            else // 입력된 문자가 영어 소문자가 아닌 경우
+            {
+                cout << c;
+            }
+            
         }
         else if (c == '\r') { // Enter 키가 눌렸을 경우
             int num = romanToInt(roman);
@@ -38,10 +47,10 @@ int main() {
             cout << "[로마자를 입력하세요]: ";
         }
         else if (c == ' ') {
-            cout << "오류발생 로마자를 입력하세요" << endl;
+            cout << "오류발생 로마자를 입력하세요: " << endl;
         }
         else if (c == '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' ) {
-            cout << "오류발생 로마자를 입력하세요" << endl;
+            cout << "오류발생 로마자를 입력하세요: " << endl;
         }
        
        

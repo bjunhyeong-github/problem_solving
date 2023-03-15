84b854c20ae8a92e7117991a893a31f1f5457620
jun <wnsgudwnsgud4724@gmail.com>
2023-03-15 20:34:14 +0900

    [BJH] 로마자를 입력받아 정수로 출력하기_실습

--- week2/roman_to_cardinal.cpp
+++ week2/roman_to_cardinal.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int romanToInt(string s) {                
     unordered_map<char, int> romanMap = 
-    { {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000} };
+    { {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000} };        
     int result = 0;
     int prev = 0;
     for (char c : s) {
